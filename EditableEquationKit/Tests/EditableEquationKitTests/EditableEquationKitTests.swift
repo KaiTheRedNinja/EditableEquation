432a9d64d4061f7f42ef76a1c515fb4c0c5ded61
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-11-04 09:34:57 +0800

    Add some tests

--- EditableEquationKit/Tests/EditableEquationKitTests/EditableEquationKitTests.swift
+++ EditableEquationKit/Tests/EditableEquationKitTests/EditableEquationKitTests.swift
@@ -2,11 +2,107 @@ import XCTest
 @testable import EditableEquationKit
 @testable import EditableEquationCore
 
-final class EditableEquationKitTests: XCTestCase {
-    func testFractions() throws {
-        let frac = FractionValue(numerator: 1, denominator: 2)
-        let frac2 = FractionValue(numerator: 3, denominator: 4)
-        let result = FractionValue(numerator: 3, denominator: 8)
-        XCTAssertEqual(frac*frac2, result)
+final class LinearGroupTests: XCTestCase {
+    let uuids = (0..<20).map({ _ in UUID() })
+
+    // TO TEST:
+    //
+    // Solving
+    // BODMAS
+    // Double negatives
+    // Implied multiplication
+
+    func testLinearGroupSimpleOptimisation() throws {
+        // optimising single linear group out
+        let initialGroup = LinearGroup(
+            id: uuids[0],
+            contents: [
+                LinearGroup(
+                    id: uuids[1],
+                    contents: [
+                        NumberToken(id: uuids[2], digit: 4),
+                        LinearOperationToken(id: uuids[3], operation: .divide),
+                        NumberToken(id: uuids[4], digit: 5)
+                    ],
+                    hasBrackets: false
+                ),
+                LinearOperationToken(id: uuids[5], operation: .plus),
+                NumberToken(id: uuids[6], digit: 8)
+            ],
+            hasBrackets: false
+        )
+
+        let finalGroup = LinearGroup(
+            id: uuids[0],
+            contents: [
+                NumberToken(id: uuids[2], digit: 4),
+                LinearOperationToken(id: uuids[3], operation: .divide),
+                NumberToken(id: uuids[4], digit: 5),
+                LinearOperationToken(id: uuids[5], operation: .plus),
+                NumberToken(id: uuids[6], digit: 8)
+            ],
+            hasBrackets: false
+        )
+
+        let optimised = initialGroup.optimised()
+        XCTAssertTrue(optimised is LinearGroup)
+        let optimisedGroup = optimised as! LinearGroup
+        XCTAssertEqual("\(optimisedGroup)", "\(finalGroup)")
+    }
+
+    func testLinearGroupAdvancedOptimisation() throws {
+        // optimising multiple layers deep, making sure not to optimise out brackets
+        let initialGroup = LinearGroup(
+            id: uuids[0],
+            contents: [
+                LinearGroup(
+                    id: uuids[1],
+                    contents: [
+                        NumberToken(id: uuids[2], digit: 4),
+                        LinearOperationToken(id: uuids[3], operation: .divide),
+                        LinearGroup(
+                            id: uuids[4],
+                            contents: [
+                                NumberToken(id: uuids[5], digit: 5)
+                            ],
+                            hasBrackets: false
+                        ),
+                    ],
+                    hasBrackets: false
+                ),
+                LinearOperationToken(id: uuids[6], operation: .plus),
+                LinearGroup(
+                    id: uuids[7],
+                    contents: [
+                        NumberToken(id: uuids[8], digit: 9)
+                    ],
+                    hasBrackets: true
+                ),
+            ],
+            hasBrackets: false
+        )
+
+        let finalGroup = LinearGroup(
+            id: uuids[0],
+            contents: [
+                NumberToken(id: uuids[2], digit: 4),
+                LinearOperationToken(id: uuids[3], operation: .divide),
+                NumberToken(id: uuids[5], digit: 5),
+                LinearOperationToken(id: uuids[6], operation: .plus),
+                LinearGroup(
+                    id: uuids[7],
+                    contents: [
+                        NumberToken(id: uuids[8], digit: 9)
+                    ],
+                    hasBrackets: true
+                )
+            ],
+            hasBrackets: false
+        )
+
+        let optimised = initialGroup.optimised()
+        XCTAssertTrue(optimised is LinearGroup)
+        let optimisedGroup = optimised as! LinearGroup
+        XCTAssertEqual("\(optimisedGroup)", "\(finalGroup)")
     }
 }
